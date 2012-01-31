847907cf0d132ea07a7e02cc939019b2aa0260fb
Eric Betts <bettse@gmail.com>
2012-01-30 23:01:47 -0800

    Initial Commit

---
+++ tagEventor/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  tagEventor
+//
+//  Created by Eric Betts on 1/30/12.
+//  Copyright 2012 ADP. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
